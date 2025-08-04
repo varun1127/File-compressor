47c0eb4969a89c75db83864ced69b323103fafe7
varun1127 <choudhryvarun370@gmail.comgit config --global user.email choudhryvarun370@gmail.in>
2025-08-05 00:48:51 +0530

    first commit

---
+++ src/main/java/prog/Main.java
@@ -0,0 +1,94 @@
+package src.main.java.prog;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+import src.main.java.prog.huffman.Hzipping;
+import src.main.java.prog.huffman.Hunzipping;
+import src.main.java.prog.lzw.Lzipping;
+import src.main.java.prog.lzw.Lunzipping;
+
+public class Main extends JFrame implements ActionListener {
+	// Definition of global values and items that are part of the GUI.
+
+	static public File opened_file, other_file;
+	static long past, future;
+	static JLabel redLabel, blueLabel, redScore, blueScore;
+	static JPanel buttonPanel, titlePanel, scorePanel;
+	static JButton ZH, UH, ZL, UL, EX;
+
+	public JPanel createContentPane() {
+		return MainPanel.createContentPane(this);
+	}
+
+	private void handleHuffmanCompression() {
+		Hzipping.beginHzipping(opened_file.getPath());
+		showCompressionCompleteDialog("Zipping");
+		updateFileStats(".huffz");
+	}
+
+	private void handleHuffmanDecompression() {
+		Hunzipping.beginHunzipping(opened_file.getPath());
+		showCompressionCompleteDialog("UnZipping");
+		updateDecompressionStats();
+	}
+
+	private void handleLZWCompression() {
+		Lzipping.beginLzipping(opened_file.getPath());
+		showCompressionCompleteDialog("Zipping");
+		updateFileStats(".LmZWp");
+	}
+
+	private void handleLZWDecompression() {
+		Lunzipping.beginLunzipping(opened_file.getPath());
+		showCompressionCompleteDialog("UnZipping");
+		updateDecompressionStats();
+	}
+
+	private void showCompressionCompleteDialog(String operation) {
+		JOptionPane.showMessageDialog(null,
+				"..........................." + operation + " Finished..........................",
+				"Status", JOptionPane.PLAIN_MESSAGE);
+	}
+
+	private void updateFileStats(String extension) {
+		redScore.setText(opened_file.length() + "Bytes");
+		other_file = new File(opened_file.getPath() + extension);
+		future = other_file.length();
+		blueScore.setText(future + "Bytes");
+	}
+
+	private void updateDecompressionStats() {
+		redScore.setText(opened_file.length() + "Bytes");
+		String s = opened_file.getPath();
+		s = s.substring(0, s.length() - 6);
+		other_file = new File(s);
+		future = other_file.length();
+		blueScore.setText(future + "Bytes");
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == ZH) {
+			handleHuffmanCompression();
+		} else if (e.getSource() == UH) {
+			handleHuffmanDecompression();
+		} else if (e.getSource() == ZL) {
+			handleLZWCompression();
+		} else if (e.getSource() == UL) {
+			handleLZWDecompression();
+		} else if (e.getSource() == EX) {
+			System.exit(0);
+		}
+	}
+
+	public static void main(String[] args) {
+		MainFrame.init();
+	}
+}
---
+++ src/test/java/prog/huffman/HuffmanCompressionIntegrationTest.java
@@ -0,0 +1,111 @@
+package src.test.java.prog.huffman;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HuffmanCompressionIntegrationTest {
+    @TempDir
+    Path tempDir;
+    private File originalFile;
+    private File compressedFile;
+    private File decompressedFile;
+    private static final String TEST_RESOURCE_PATH = "src/test/resources/large_test_file.txt";
+
+    @BeforeEach
+    void setUp() throws IOException {
+        // Create a copy of the test file in the temp directory
+        originalFile = tempDir.resolve("large_test_file.txt").toFile();
+        Files.copy(Path.of(TEST_RESOURCE_PATH), originalFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        
+        compressedFile = new File(originalFile.getAbsolutePath() + ".huffz");
+        decompressedFile = new File(originalFile.getAbsolutePath());
+    }
+
+    @Test
+    void testLargeFileCompression() throws IOException {
+        // Store original file size and content
+        long originalSize = Files.size(originalFile.toPath());
+        String originalContent = Files.readString(originalFile.toPath());
+        System.out.println("Original file size: " + originalSize + " bytes");
+
+        // Compress the file
+        Hzipping.beginHzipping(originalFile.getAbsolutePath());
+        assertTrue(compressedFile.exists(), "Compressed file should exist");
+        
+        long compressedSize = Files.size(compressedFile.toPath());
+        System.out.println("Compressed file size: " + compressedSize + " bytes");
+        System.out.println("Compression ratio: " + String.format("%.2f%%", (100.0 * compressedSize / originalSize)));
+
+        // Delete original file to ensure we're reading from decompressed file
+        Files.delete(originalFile.toPath());
+        assertFalse(originalFile.exists(), "Original file should be deleted");
+
+        // Decompress
+        Hunzipping.beginHunzipping(compressedFile.getAbsolutePath());
+        assertTrue(decompressedFile.exists(), "Decompressed file should exist");
+
+        // Compare contents
+        String decompressedContent = Files.readString(decompressedFile.toPath());
+        assertEquals(originalContent, decompressedContent, "Decompressed content should match original");
+        
+        // Compare file sizes
+        long decompressedSize = Files.size(decompressedFile.toPath());
+        assertEquals(originalSize, decompressedSize, "Decompressed file size should match original");
+        
+        // Print compression statistics
+        System.out.println("Decompressed file size: " + decompressedSize + " bytes");
+        System.out.println("Files are identical: " + originalContent.equals(decompressedContent));
+    }
+
+    @Test
+    void testSimpleText() throws IOException {
+        // Write simple content
+        Files.writeString(originalFile.toPath(), "Hello, World!");
+        testCompressionCycle(originalFile);
+    }
+
+    @Test
+    void testEmptyFile() throws IOException {
+        // Write empty content
+        Files.writeString(originalFile.toPath(), "");
+        testCompressionCycle(originalFile);
+    }
+
+    @Test
+    void testSpecialCharacters() throws IOException {
+        // Write content with special characters
+        Files.writeString(originalFile.toPath(), "!@#$%^&*()_+{}[]|\\:;\"'<>,.?/~`\n\t");
+        testCompressionCycle(originalFile);
+    }
+
+    private void testCompressionCycle(File testFile) throws IOException {
+        long originalSize = Files.size(testFile.toPath());
+        String originalContent = Files.readString(testFile.toPath());
+
+        // Compress
+        Hzipping.beginHzipping(testFile.getAbsolutePath());
+        assertTrue(compressedFile.exists(), "Compressed file should exist");
+
+        // Delete original file
+        Files.delete(testFile.toPath());
+        assertFalse(testFile.exists(), "Original file should be deleted");
+
+        // Decompress
+        Hunzipping.beginHunzipping(compressedFile.getAbsolutePath());
+        assertTrue(decompressedFile.exists(), "Decompressed file should exist");
+
+        // Compare contents
+        String decompressedContent = Files.readString(decompressedFile.toPath());
+        assertEquals(originalContent, decompressedContent, "Decompressed content should match original");
+        assertEquals(originalSize, Files.size(decompressedFile.toPath()), "File sizes should match");
+    }
+} 
